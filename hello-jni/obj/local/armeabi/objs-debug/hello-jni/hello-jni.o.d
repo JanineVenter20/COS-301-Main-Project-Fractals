08e77922c44ea3f3c455ae593916a0aceb897bff
Janine Venter <janineve@gmail.com>
2013-08-20 19:34:19 +0200

    Sample for using JNI
    This is one of the samples given by the ndk environment, we used this
    just to test out how C++ and java, works together in the android
    environment

---
+++ hello-jni/obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ jni/string.h \
+ C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h \
+ jni/jni.h
+
+jni/string.h:
+
+C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/Janine/Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+jni/jni.h:
