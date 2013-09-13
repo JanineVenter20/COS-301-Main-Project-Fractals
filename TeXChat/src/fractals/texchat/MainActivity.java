f19115aa408521c6995e67a9a78816eedc77dac9
Stephan <kalaharib@gmail.com>
2013-09-13 13:58:47 +0200

    inline images in messages
    message sending not really working right anymore

--- TeXChat/src/fractals/texchat/MainActivity.java
+++ TeXChat/src/fractals/texchat/MainActivity.java
@@ -34,14 +34,10 @@ public class MainActivity extends Activity {
 	final static int port = 5222;
 	
 	
-//	static {
-//		System.loadLibrary("chatTex") ;
-//	}
-	
 	public static final ConnectionConfiguration ccf = new ConnectionConfiguration(hostName, port, service);
 	public static final XMPPConnection conn = new XMPPConnection(ccf);
 	Roster roster;
-	Chat activeChat;
+	public static Chat activeChat;
 	ChatManager cm;
 	ListView contactLV;
 	Context context = this;
@@ -51,15 +47,15 @@ public class MainActivity extends Activity {
 	ChatManagerListener cml = new ChatManagerListener() {			 
 		public void chatCreated(Chat chat, boolean locally) {
 			if (!locally) {
-				activeChat = cm.createChat(chat.getParticipant(), "test", ml);
+				if (activeChat != null)
+				{
+					activeChat = chat;
+					System.out.println(activeChat.toString());
+					
+				} else {
+					activeChat = cm.createChat(chat.getParticipant(), null);
+				}
 			}
-		}
-	};
-	
-	MessageListener ml = new MessageListener() {			
-		@Override
-		public void processMessage(Chat arg0, Message arg1) {
-			
 		}
 	};
 	
--- TeXChat/src/fractals/texchat/TexActivity.java
+++ TeXChat/src/fractals/texchat/TexActivity.java
@@ -1,8 +1,11 @@
 package fractals.texchat;
 
+import java.io.ObjectOutputStream.PutField;
+
 import fractals.texchat.R.id;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.view.Menu;
 import android.view.View;
@@ -27,14 +30,13 @@ public class TexActivity extends Activity {
 		getMenuInflater().inflate(R.menu.tex, menu);
 		
 		Button viewb = (Button)findViewById(id.viewButton);
-		
-		viewb.setOnClickListener(ocl);
-		
-		
+		Button okButton = (Button)findViewById(id.okButton);
+		viewb.setOnClickListener(viewOnclick);
+		okButton.setOnClickListener(okOnclick);
 		return true;
 	}
 	
-	OnClickListener ocl = new OnClickListener() {
+	OnClickListener viewOnclick = new OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
@@ -44,6 +46,24 @@ public class TexActivity extends Activity {
 			ImageView iv = (ImageView)findViewById(id.imageView1);
 			iv.setImageBitmap(bmp);
 		}
+	};
+	
+	OnClickListener okOnclick = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Intent i = new Intent();
+			EditText ed= (EditText)findViewById(id.expEdit);
+			i.putExtra("expression", ed.getText().toString());
+			setResult(RESULT_OK, i);
+			finish();
+		}
+	};
+	
+	protected void onDestroy() {
+		super.onDestroy();
+		setResult(RESULT_CANCELED);
+		finish();
 	};
 
 }
